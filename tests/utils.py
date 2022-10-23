efafcc8dcb9598ef98d8c839422e8f44e6377bfa
Marcin Morys <marcin.morys.1@us.af.mil>
2022-10-23 14:20:15 -0400

    chore: :construction: add utils functions for testing

--- tests/utils.py
+++ tests/utils.py
@@ -26,9 +26,44 @@ def build_project(*args, **kwargs):
     _run_command(sys.executable, "-m", "hatchling", "build", *args, env=env)
 
 
+def build_ext(*args, **kwargs):
+    env = kwargs.get("env", os.environ.copy())
+    _run_command(sys.executable, "-m", "hatchling", "build", *args, env=env)
+
+
 def _run_command(*command, **kwargs):  # sourcery skip: raise-specific-error
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kwargs)  # noqa: S603
     stdout, _ = process.communicate()
 
     if process.returncode:  # no cov
         raise Exception(stdout.decode("utf-8"))
+
+
+def file_exists(path):
+    path = pathlib.Path(path)
+    return path.exists() and path.is_file()
+
+
+def directory_exists(path):
+    path = pathlib.Path(path)
+    return path.exists() and path.is_dir()
+
+
+def content_with_prefix_exists(parent_dir, prefix, _type=None):
+    # sourcery skip: reintroduce-else, remove-redundant-continue, use-next
+    parent_dir = pathlib.Path(parent_dir)
+    type_validators = {"file": "is_file", "dir": "is_dir", "directory": "is_directory"}
+    for content in parent_dir.iterdir():
+        if _type and not getattr(content, type_validators[_type])():
+            continue
+        if content.name.startswith(prefix):
+            return True
+    return False
+
+
+def directory_with_prefix_exists(parent_dir, prefix):
+    return content_with_prefix_exists(parent_dir, prefix, _type="dir")
+
+
+def file_with_prefix_exists(parent_dir, prefix):
+    return content_with_prefix_exists(parent_dir, prefix, _type="file")
